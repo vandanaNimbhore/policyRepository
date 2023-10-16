cdfea7afc2e4557193893a9dc5caf44ddc5bdd35
yogitashinde88 <yogitashinde88gmail.com>
2023-10-16 11:18:44 +0200

    [Yogitashinde88] added new task

---
+++ src/test/java/policyTestPackage/Yogis_task.java
@@ -0,0 +1,10 @@
+package policyTestPackage;
+
+public class Yogis_task {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("Task completed");
+	}
+
+}
