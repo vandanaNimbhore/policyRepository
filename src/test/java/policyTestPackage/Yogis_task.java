7c96d1ac7b4174b5d7408d73c190c4aeb98bfea5
yogitashinde88 <yogitashinde88gmail.com>
2023-10-16 11:33:53 +0200

    [Yogitashinde88] waits added

--- src/test/java/policyTestPackage/Yogis_task.java
+++ src/test/java/policyTestPackage/Yogis_task.java
@@ -2,10 +2,11 @@ package policyTestPackage;
 
 public class Yogis_task {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
     System.out.println("Task completed");
     System.out.println(" One update");
+    Thread.sleep(3000);
 	}
 
 }
